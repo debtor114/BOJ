c2856fb0a6163414775f0c6bbea503ebd0bb95f1
doooooooong <21500002@handong.edu>
2021-09-05 23:47:35 +0900

    [UPDATE] at least 1

---
+++ etc/1871.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        string s;
+        cin >> s;
+
+        string alpha, num;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (isalpha(s[i])) alpha.push_back(s[i]);
+            if (isnumber(s[i])) num.push_back(s[i]);
+        }
+
+        int n1 = 0, n2 = 0;
+
+        for (int i = 0; i < 3; i++) {
+            n1 += (s[2-i]-65)*pow(26, i);
+        }
+
+        n2 = stoi(num);
+
+        if (abs(n1-n2) <= 100) cout << "nice" << "\n";
+        else cout << "not nice" << "\n";
+    }
+
+    return 0;
+
+}
